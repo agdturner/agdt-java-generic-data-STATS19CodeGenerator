c86e15f34037170f961f4ac99bc1421959656427
Andy Turner <A.G.D.Turner@leeds.ac.uk>
2020-02-15 17:08:12 +0000

    Code rationalisation for 1.0 release.

--- src/main/java/uk/ac/leeds/ccg/data/stats19/process/STATS19_JavaCodeGenerator.java
+++ src/main/java/uk/ac/leeds/ccg/data/stats19/process/STATS19_JavaCodeGenerator.java
@@ -21,11 +21,7 @@ import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.TreeSet;
 import uk.ac.leeds.ccg.cg.process.CG_Process;
 import uk.ac.leeds.ccg.data.Data_VariableType;
 import uk.ac.leeds.ccg.data.Data_VariableType.Data_VariableNamesAndTypes;
@@ -33,7 +29,6 @@ import uk.ac.leeds.ccg.data.core.Data_Environment;
 import uk.ac.leeds.ccg.generic.core.Generic_Environment;
 import uk.ac.leeds.ccg.generic.io.Generic_IO;
 import uk.ac.leeds.ccg.data.stats19.core.STATS19_Environment;
-import uk.ac.leeds.ccg.data.stats19.core.STATS19_Object;
 import uk.ac.leeds.ccg.data.stats19.core.STATS19_Strings;
 import uk.ac.leeds.ccg.generic.io.Generic_Defaults;
 
@@ -81,31 +76,46 @@ public class STATS19_JavaCodeGenerator extends CG_Process {
     public void run() throws FileNotFoundException, IOException {
         vt = new Data_VariableType(de);
         vt.setDelimiter(",");
-        String type;
+        int dp = 5;
+        Path indir = env.files.getInputDir();
+        int startYear = 2009;
+        int endYear = 2018;
+        int nYears = endYear - startYear + 1;
+        /**
+         * Accident data
+         */
+        Path[] afs = new Path[nYears];
+        for (int year = startYear; year <= endYear; year ++) {
+            afs[year - startYear] = Paths.get(indir.toString(), "Accidents_" 
+                    + Integer.toString(year) + ".csv");
+        }
+        Data_VariableNamesAndTypes avnt = vt.getVariableNamesAndTypes(100, afs, 
+                dp, 1, "t");
+        run("accident", avnt);
 
         /**
-         *
+         * Casualty data
          */
-        int dp = 5;
+        Path[] cfs = new Path[nYears];
+        for (int year = startYear; year <= endYear; year ++) {
+            cfs[year - startYear] = Paths.get(indir.toString(), "Casualties_" 
+                    + Integer.toString(year) + ".csv");
+        }
+        Data_VariableNamesAndTypes cvnt = vt.getVariableNamesAndTypes(100, cfs, 
+                dp, 1, "t");
+        run("casualty", cvnt);
 
-        Path indir = env.files.getInputDir();
-        Path[] fs = new Path[2];
-        fs[0] = Paths.get(indir.toString(), "Accidents_2018.csv");
-        fs[1] = Paths.get(indir.toString(), "Accidents_2017.csv");
-        //t = getFieldTypes(Integer.MAX_VALUE, fs, dp);
-        Data_VariableNamesAndTypes vnt = vt.getFieldTypes(100, fs, dp, 1, "t");
-        type = "accident";
-        run(type, vnt);
-
-//        type = "casualty";
-//        f = getInputFile(type, files.getInputDataDir());
-//        t = getFieldTypes(f, dp);
-//        run(type, t);
-//
-//        type = "vehicle";
-//        f = getInputFile(type, files.getInputDataDir());
-//        t = getFieldTypes(f, dp);
-//        run(type, t);
+        /**
+         * Vehicle data
+         */
+        Path[] vfs = new Path[nYears];
+        for (int year = startYear; year <= endYear; year ++) {
+            vfs[year - startYear] = Paths.get(indir.toString(), "Vehicles_" 
+                    + Integer.toString(year) + ".csv");
+        }
+        Data_VariableNamesAndTypes vvnt = vt.getVariableNamesAndTypes(100, vfs, 
+                dp, 1, "t");
+        run("vehicle", vvnt);    
     }
 
     public void run(String type, Data_VariableNamesAndTypes vnt) throws IOException {
@@ -119,40 +129,28 @@ public class STATS19_JavaCodeGenerator extends CG_Process {
         String className = prepend + "_" + type + "_Record";
         Path fout = Paths.get(outdir.toString(), className + ".java");
         try (PrintWriter pw = Generic_IO.getPrintWriter(fout, false)) {
-            writeHeaderPackageAndImports(pw, packageName, "", vnt);
+            writeHeaderPackageAndImports(pw, packageName, null, vnt);
             printClassDeclarationSerialVersionUID(pw, packageName, className, "", "");
-            // Print Field Declarations Inits And Getters
-            // initialise type2TypeName
+            /**
+             * Print Field Declarations Inits And Getters
+             * ------------------------------------------
+             */
+            /**
+             * Deal with the first field which for some reason has an extra
+             * characters at at the start of the field name. Deal with it be 
+             * removing this character
+             */
+            if (true) {
+                String field = vnt.order2FieldNames.get(0);
+                String newName = field.substring(0,1) + field.substring(2);
+                System.out.println(newName);
+                vnt.order2FieldNames.put(0, newName);
+                vnt.fieldNames2Order.put(newName, 0);
+            }
             HashMap<Integer, String> type2TypeName = vt.getType2TypeName();
-            //vnt.fieldNames2Order;
-            // Print field declaration
-            Iterator<Integer> ite = vnt.order2FieldNames.keySet().iterator();
-            while (ite.hasNext()) {
-                int index = ite.next();
-                String field = vnt.order2FieldNames.get(index);
-                int typ = vnt.order2Type.get(index);
-                String typeName = type2TypeName.get(typ);
-                // print field declaration
-                pw.println("protected " + typeName + " " + field + ";");
-            }
-            
-            // Print inits
-            ite = vnt.order2FieldNames.keySet().iterator();
-            while (ite.hasNext()) {
-                int index = ite.next();
-                String field = vnt.order2FieldNames.get(index);
-                int typ = vnt.order2Type.get(index);
-                String typeName = type2TypeName.get(typ);
-
-//                printFieldDeclarationsInitsAndGetters(pw, fields, fieldTypes, v0);
-//                // Constructor
-//                pw.println("public " + className + "(String line) {");
-//                pw.println("s = line.split(\"\\t\");");
-//                for (int j = 0; j < headers[w].length; j++) {
-//                    pw.println("init" + headers[w][j] + "(s[" + j + "]);");
-//                }
-            }
-            pw.println("}");
+            printFieldDeclarations(pw, type2TypeName, vnt);
+            printFieldInits(pw, vnt);
+            printFieldGetters(pw, type2TypeName, vnt);
             pw.println("}");
         }
 
@@ -177,371 +175,5 @@ public class STATS19_JavaCodeGenerator extends CG_Process {
         //printGetID(pw);
         pw.println("}");
     }
-    
-    /**
-     *
-     * @param pw
-     * @param packageName
-     * @param imports
-     */
-    public void writeHeaderPackageAndImports(PrintWriter pw,
-            String packageName, String imports, Data_VariableNamesAndTypes vnt) {
-        pw.println("/**");
-        pw.println(" * Source code generated by " + this.getClass().getName());
-        pw.println(" */");
-        pw.println("package " + packageName + ";");
-        if (!imports.isEmpty()) {
-            pw.println("import " + imports + ";");
-        }
-        boolean hasBigDecimals = false;
-        for (int i = 0; i < vnt.bigDecimals.length; i ++) {
-            if (vnt.bigDecimals[i]) {
-                hasBigDecimals = true;
-                break;
-            }
-        }
-        if (hasBigDecimals) {
-            pw.println("import java.math.BigDecimal;");
-        }
-        boolean hasBigIntegers = false;
-        for (int i = 0; i < vnt.bigIntegers.length; i ++) {
-            if (vnt.bigIntegers[i]) {
-                hasBigIntegers = true;
-                break;
-            }
-        }
-        if (hasBigIntegers) {
-            pw.println("import java.math.BigInteger;");
-        }
-    }
 
-    /**
-     *
-     * @param pw
-     * @param packageName
-     * @param className
-     * @param implementations
-     * @param extendedClassName
-     */
-    public void printClassDeclarationSerialVersionUID(PrintWriter pw,
-            String packageName, String className, String implementations,
-            String extendedClassName) {
-        pw.print("public class " + className);
-        if (!extendedClassName.isEmpty()) {
-            pw.print(" extends " + extendedClassName);
-        }
-        if (!implementations.isEmpty()) {
-            pw.print(" implements " + implementations);
-        }
-        pw.println(" {");
-        /**
-         * This is not included for performance reasons. pw.println("private
-         * static final long serialVersionUID = " + serialVersionUID + ";");
-         */
-    }
-
-    /**
-     * @param pw
-     * @param fields
-     * @param fieldTypes
-     * @param v0
-     */
-    public void printFieldDeclarationsInitsAndGetters(PrintWriter pw,
-            TreeSet<String> fields, HashMap<String, Integer> fieldTypes,
-            HashMap<String, Byte> v0) {
-        // Field declarations
-        printFieldDeclarations(pw, fields, fieldTypes);
-        // Field init
-        printFieldInits(pw, fields, fieldTypes, v0);
-        // Field getters
-        printFieldGetters(pw, fields, fieldTypes);
-    }
-
-    /**
-     * @param pw
-     * @param fields
-     * @param fieldTypes
-     */
-    public void printFieldDeclarations(PrintWriter pw, TreeSet<String> fields,
-            HashMap<String, Integer> fieldTypes) {
-        String field;
-        int fieldType;
-        Iterator<String> ite;
-        ite = fields.iterator();
-        while (ite.hasNext()) {
-            field = ite.next();
-            fieldType = fieldTypes.get(field);
-            switch (fieldType) {
-                case 0:
-                    pw.println("protected String " + field + ";");
-                    break;
-                case 1:
-                    pw.println("protected double " + field + ";");
-                    break;
-                case 2:
-                    pw.println("protected int " + field + ";");
-                    break;
-                case 3:
-                    pw.println("protected short " + field + ";");
-                    break;
-                case 4:
-                    pw.println("protected byte " + field + ";");
-                    break;
-                default:
-                    pw.println("protected boolean " + field + ";");
-                    break;
-            }
-        }
-    }
-
-    /**
-     *
-     * @param pw
-     * @param fields
-     * @param fieldTypes
-     */
-    public void printFieldGetters(PrintWriter pw, TreeSet<String> fields,
-            HashMap<String, Integer> fieldTypes) {
-        String field;
-        int fieldType;
-        Iterator<String> ite;
-        ite = fields.iterator();
-        while (ite.hasNext()) {
-            field = ite.next();
-            fieldType = fieldTypes.get(field);
-            switch (fieldType) {
-                case 0:
-                    pw.println("public String get" + field + "() {");
-                    break;
-                case 1:
-                    pw.println("public double get" + field + "() {");
-                    break;
-                case 2:
-                    pw.println("public int get" + field + "() {");
-                    break;
-                case 3:
-                    pw.println("public short get" + field + "() {");
-                    break;
-                case 4:
-                    pw.println("public byte get" + field + "() {");
-                    break;
-                default:
-                    pw.println("public boolean get" + field + "() {");
-                    break;
-            }
-            pw.println("return " + field + ";");
-            pw.println("}");
-            pw.println();
-        }
-    }
-
-    /**
-     *
-     * @param pw
-     * @param fields
-     * @param fieldTypes
-     * @param v0
-     */
-    public void printFieldInits(PrintWriter pw, TreeSet<String> fields,
-            HashMap<String, Integer> fieldTypes, HashMap<String, Byte> v0) {
-        String field;
-        int fieldType;
-        Iterator<String> ite;
-        ite = fields.iterator();
-        while (ite.hasNext()) {
-            field = ite.next();
-            fieldType = fieldTypes.get(field);
-            pw.println("protected final void init" + field + "(String s) {");
-            pw.println("if (!s.trim().isEmpty()) {");
-            switch (fieldType) {
-                    case 0:
-                    pw.println(field + " = s;");
-                    break;
-                case 1:
-                    pw.println(field + " = Double.parseDouble(s);");
-                    pw.println("} else {");
-                    pw.println(field + " = Double.NaN;");
-                    break;
-                case 2:
-                    pw.println(field + " = Integer.parseInt(s);");
-                    pw.println("} else {");
-                    pw.println(field + " = Integer.MIN_VALUE;");
-                    break;
-                case 3:
-                    pw.println(field + " = Short.parseShort(s);");
-                    pw.println("} else {");
-                    pw.println(field + " = Short.MIN_VALUE;");
-                    break;
-                case 4:
-                    pw.println(field + " = Byte.parseByte(s);");
-                    pw.println("} else {");
-                    pw.println(field + " = Byte.MIN_VALUE;");
-                    break;
-                default:
-                    pw.println("byte b = Byte.parseByte(s);");
-                    if (v0.get(field) == null) {
-                        pw.println(field + " = false;");
-                    } else {
-                        pw.println("if (b == " + v0.get(field) + ") {");
-                        pw.println(field + " = false;");
-                        pw.println("} else {");
-                        pw.println(field + " = true;");
-                        pw.println("}");
-                    }
-                    break;
-            }
-            pw.println("}");
-            pw.println("}");
-            pw.println();
-        }
-    }
-
-    protected HashMap<String, Byte> setCommonBooleanMaps(
-            HashMap<String, Byte>[] v0ms,
-            HashMap<String, Byte>[] v1ms,
-            TreeSet<String>[] allFields,
-            HashMap<String, Integer> fieldTypes) {
-        HashMap<String, Byte> v0m;
-        HashMap<String, Byte> v1m;
-        Iterator<String> ites0;
-        Iterator<String> ites1;
-        String field0;
-        String field1;
-        TreeSet<String> fields;
-        fields = allFields[5];
-        HashMap<String, Byte> v0m1;
-        HashMap<String, Byte> v1m1;
-        v0m1 = new HashMap<>();
-        v1m1 = new HashMap<>();
-        ites0 = fields.iterator();
-        byte v0;
-        Byte v1;
-        Byte v01;
-        Byte v11;
-        while (ites0.hasNext()) {
-            field0 = ites0.next();
-            if (fieldTypes.get(field0) == 5) {
-                for (int w = 0; w < v0ms.length; w++) {
-                    v0m = v0ms[w];
-                    v1m = v1ms[w];
-                    ites1 = v0m.keySet().iterator();
-                    while (ites1.hasNext()) {
-                        field1 = ites1.next();
-                        if (field0.equalsIgnoreCase(field1)) {
-                            v0 = v0m.get(field1);
-                            if (v1m == null) {
-                                v1 = Byte.MIN_VALUE;
-                            } else {
-                                //System.out.println("field1 " + field1);
-                                //System.out.println("field1 " + field1);
-                                v1 = v1m.get(field1);
-                                if (v1 == null) {
-                                    v1 = Byte.MIN_VALUE;
-                                }
-                            }
-                            v01 = v0m1.get(field1);
-                            v11 = v1m1.get(field1);
-                            if (v01 == null) {
-                                v0m1.put(field1, v0);
-                            } else {
-                                if (v01 != v0) {
-                                    // Field better stored as a byte than boolean.
-                                    fieldTypes.put(field1, 4);
-                                }
-                                if (v11 == null) {
-                                    v1m1.put(field1, v1);
-                                } else {
-                                    if (v1 != v11.byteValue()) {
-                                        // Field better stored as a byte than boolean.
-                                        fieldTypes.put(field1, 4);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return v0m1;
-    }
-
-    /**
-     * Finds and returns those fields that are in common and those fields .
-     * result[0] are the fields in common with all.
-     *
-     * @param headers
-     * @return
-     */
-    public ArrayList<String>[] getFieldsList(ArrayList<String> headers) {
-        ArrayList<String>[] r;
-        int size;
-        size = headers.size();
-        r = new ArrayList[size];
-        Iterator<String> ite;
-        ite = headers.iterator();
-        int i;
-        i = 0;
-        while (ite.hasNext()) {
-            r[i] = getFieldsList(ite.next());
-            i++;
-        }
-        return r;
-    }
-
-    /**
-     *
-     * @param fields
-     * @return
-     */
-    public TreeSet<String> getFields(String[] fields) {
-        TreeSet<String> r;
-        r = new TreeSet<>();
-        r.addAll(Arrays.asList(fields));
-        return r;
-    }
-
-    /**
-     *
-     * @param s
-     * @return
-     */
-    public ArrayList<String> getFieldsList(String s) {
-        ArrayList<String> r;
-        r = new ArrayList<>();
-        String[] split;
-        split = s.split("\t");
-        r.addAll(Arrays.asList(split));
-        return r;
-    }
-
-    /**
-     * Returns all the values common to s1, s2, s3, s4 and s5 and removes all
-     * these common fields from s1, s2, s3, s4 and s5.
-     *
-     * @param s1
-     * @param s2
-     * @param s3 May be null.
-     * @param s4 May be null.
-     * @param s5 May be null.
-     * @return
-     * @Todo generalise
-     */
-    public TreeSet<String> getFieldsInCommon(TreeSet<String> s1,
-            TreeSet<String> s2, TreeSet<String> s3, TreeSet<String> s4,
-            TreeSet<String> s5) {
-        TreeSet<String> result;
-        result = new TreeSet<>();
-        result.addAll(s1);
-        result.retainAll(s2);
-        if (s3 != null) {
-            result.retainAll(s3);
-        }
-        if (s4 != null) {
-            result.retainAll(s4);
-        }
-        if (s5 != null) {
-            result.retainAll(s5);
-        }
-        return result;
-    }
 }
